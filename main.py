bab86f168e7b1c5c26c1232ee7d636ad5ab8e3bb
omkar burute <buruteomkar90@gmail.com>
2024-12-13 20:08:42 +0530

    Started With Leetcode75. Quest no 1768

--- main.py
+++ main.py
@@ -8,3 +8,16 @@ Question no:1768
 You are given two strings word1 and word2. Merge the strings by adding letters in alternating order,
 starting with word1. If a string is longer than the other, append the additional letters onto the end of the merged string.
 """
+class Solution(object):
+    def mergeAlternately(self, word1, word2):
+        """
+        :type word1: str
+        :type word2: str
+        :rtype: str
+        """
+        word3 = ""
+        for i, j in zip(word1, word2):
+            word3 += i + j
+        word3 += word1[len(word2):] + word2[len(word1):]
+        return word3
+
