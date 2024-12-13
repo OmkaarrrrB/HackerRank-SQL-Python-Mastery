3c4c6acc0ffab67d82f973870ebd2187b3e9b91f
omkar burute <buruteomkar90@gmail.com>
2024-12-13 20:07:59 +0530

    Started With Leetcode75. Quest no 1768

---
+++ main.py
@@ -0,0 +1,10 @@
+#Leetcode70
+
+"""
+Question Link
+https://leetcode.com/problems/merge-strings-alternately/description/?envType=study-plan-v2&envId=leetcode-75
+
+Question no:1768
+You are given two strings word1 and word2. Merge the strings by adding letters in alternating order,
+starting with word1. If a string is longer than the other, append the additional letters onto the end of the merged string.
+"""
